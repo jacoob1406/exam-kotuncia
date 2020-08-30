66f6c959c7dc3cc360d6c357802b2efb7a49ba89
Jakub Lemka <jakub.lemka@lppsa.com>
2020-08-30 16:36:32 +0200

    Updates

---
+++ precache-manifest.d8fc796835377a2df288260704e61ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca55c8935433856b8931298df6eeb651",
+    "url": "/exam-kotuncia/index.html"
+  },
+  {
+    "revision": "afcee2d9bea581fdea22",
+    "url": "/exam-kotuncia/static/css/main.6318d0d9.chunk.css"
+  },
+  {
+    "revision": "43ec451d0fd8693ff0ba",
+    "url": "/exam-kotuncia/static/js/2.fb568156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exam-kotuncia/static/js/2.fb568156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcee2d9bea581fdea22",
+    "url": "/exam-kotuncia/static/js/main.1a4348b5.chunk.js"
+  },
+  {
+    "revision": "de0d556ee05c09b11be1",
+    "url": "/exam-kotuncia/static/js/runtime-main.dea1d0b1.js"
+  }
+]);
